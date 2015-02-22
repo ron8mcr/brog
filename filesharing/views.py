8c7bf9f0a841f9d45ca0d996988cf570f622a26e
ron8mcr <rons_mail@mail.ru>
2015-02-22 19:02:53 +0700

    view для файлов и каталогов

--- filesharing/views.py
+++ filesharing/views.py
@@ -59,13 +59,27 @@ class DirCreate(CreateView):
         return HttpResponseRedirect(self.success_url)
 
 
-#TODO: переделать
 class FilesView(FormMixin, TemplateView):
     errors = {
         'BAD_PATH': "Введенный путь не существует",
         'ACCESS_DENIED': "Вам сюда доступ запрещён"
     }
     template_name = 'home.html'
+
+    def navigation_inform(self):
+        """
+        :return:информация для полей навигации
+        """
+        result = dict()
+
+        # папки, составляющие полный пусть (для СТРОКИ навигации)
+        result['path_dirs'] = self.cur_dir.get_ancestors(
+            include_self=True)
+
+        # дерево папок пользователя - для ДЕРЕВА навигации
+        result['user_dirs'] = self.request.user.home_directory.get_descendants(include_self=True)
+        result['cur_dir'] = self.cur_dir
+        return result
 
     def list_dir(self):
         """
@@ -76,8 +90,20 @@ class FilesView(FormMixin, TemplateView):
             content = dict()
             content['files'] = File.objects.filter(parent=self.cur_dir)
             content['dirs'] = Directory.objects.filter(parent=self.cur_dir)
-            content['parent'] = self.cur_dir.parent
             return content
+        else:
+            return {'error': self.errors['ACCESS_DENIED']}
+
+    def list_file(self):
+        """
+        :return:информация по запрошенному файлу или ошибка
+        """
+        result = dict()
+        # родительская директория
+        self.cur_dir = self.cur_file.parent
+
+        if self.cur_file.has_access(self.request.user):
+            return {'file': self.cur_file}
         else:
             return {'error': self.errors['ACCESS_DENIED']}
 
@@ -91,26 +117,22 @@ class FilesView(FormMixin, TemplateView):
             # список файлов и папок в текущей директории
             context.update(self.list_dir())
 
-            # папки, составляющие полный пусть (для СТРОКИ навигации)
-            context['path_dirs'] = self.cur_dir.get_ancestors(
-                include_self=True)
-
-            # дерево папок пользователя - для ДЕРЕВА навигации
-            context['user_dirs'] = self.request.user.home_directory.get_descendants(include_self=True)
-            context['cur_dir'] = self.cur_dir
+            # информация для навигации
+            context.update(self.navigation_inform())
 
             context['CreateDirForm'] = self.get_form(CreateDirectoryForm)
             context['UploadFileForm'] = self.get_form(UploadFileForm)
+            # TODO  формы удаления и переименования директории
         else:
             # если по запрошенному пати найден файл
             self.cur_file = File.objects.get_by_full_path(path)
             if self.cur_file:
-                # TODO: что же делать, если запрошен файл? redirect?
-                if self.cur_file.has_access(self.request.user):
-                    context['file'] = self.cur_file
-                else:
-                    context['error'] = self.errors['ACCESS_DENIED']
+                context.update(self.list_file())
+                context.update(self.navigation_inform())
 
+                # TODO: формы другие должны быть (переименования, удаления)
+                context['CreateDirForm'] = self.get_form(CreateDirectoryForm)
+                context['UploadFileForm'] = self.get_form(UploadFileForm)
             else:
                 context['error'] = self.errors['BAD_PATH']
         return context
