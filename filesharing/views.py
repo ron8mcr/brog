1e77032d33655e5c338dbf63d3207c9d3c275a61
ron8mcr <rons_mail@mail.ru>
2015-02-25 00:11:14 +0700

    bla

--- filesharing/views.py
+++ filesharing/views.py
@@ -43,20 +43,26 @@ class FileUpload(CreateView):
             messages.add_message(self.request, messages.ERROR, 'Вы не имеете право загружать файлы в данную директорию' )
         return HttpResponseRedirect(self.success_url)
 
+
 # TODO: разобраться, что происходит в случае ошибки и как это обрабатывать
 class DirCreate(CreateView):
     form_class = CreateDirectoryForm
-    template_name = 'home.html'
-    success_url = 'home'
+
+    def get_initial(self):
+        return {
+            'owner': self.request.user,
+            'parent': Directory.objects.get_by_full_path(self.kwargs['path'])
+        }
 
     def form_valid(self, form):
-        #получаем родительскую директорию
-        my_parent_path = self.kwargs['path']
-        self.success_url = my_parent_path
-        # говорим, чтобы view не торопилас сохранять директорию
+        # получаем объект на основе данных формы
         instance = form.save(commit=False)
-        #получаем будущего родителя
-        instance.parent = Directory.objects.get_by_full_path(my_parent_path)
+
+        # заполняем неполученные поля
+        parent_path = self.kwargs['path']
+        self.success_url = parent_path
+        instance.parent = Directory.objects.get_by_full_path(parent_path)
+        instance.owner = self.request.user
 
         if instance.parent.has_access(self.request.user):
             # назначаем владельца
@@ -64,11 +70,13 @@ class DirCreate(CreateView):
 
             # А теперь можно сохранить в базу
             instance.save()
-            messages.add_message(self.request, messages.SUCCESS, 'Директория "' + instance.name.encode('utf-8') + '" успешно создана' )
+            messages.add_message(self.request, messages.SUCCESS, 'Директория "' + instance.name + '" успешно создана' )
         else:
             messages.add_message(self.request, messages.ERROR, 'Вы не имеете право создавать директории в этой папке' )
+        return super(DirCreate, self).form_valid(form)
 
-        return HttpResponseRedirect(self.success_url)
+    def form_invalid(self, form):
+        return HttpResponse("form error")
 
 
 class DirUpdate(UpdateView):
@@ -128,6 +136,10 @@ class DirDelete(DeleteView):
 
 
 class FilesView(FormMixin, TemplateView):
+    errors = {
+        'BAD_PATH': "Введенный путь не существует",
+        'ACCESS_DENIED': "Вам сюда доступ запрещён"
+    }
     template_name = 'home.html'
 
     def navigation_inform(self):
@@ -145,79 +157,58 @@ class FilesView(FormMixin, TemplateView):
         result['cur_dir'] = self.cur_dir
         return result
 
-    def list_dir(self):
-        """
-        :return:Список файлов и директорий в запрошенной директории, если есть доступ
-                или ошибку
-        """
-        if self.cur_dir.has_access(self.request.user):
-            content = dict()
-            content['files'] = File.objects.filter(parent=self.cur_dir)
-            content['dirs'] = Directory.objects.filter(parent=self.cur_dir)
-            return content
+    def prepare_dir_context(self, context):
+        # список файлов и папок в текущей директории
+        if not self.cur_dir.has_access(self.request.user):
+            context['critical_error'] = self.errors['ACCESS_DENIED']
         else:
-            messages.add_message(self.request, messages.ERROR, 'Вам сюда доступ запрещён' )
-            return dict()
+            context['files'] = File.objects.filter(parent=self.cur_dir)
+            context['dirs'] = Directory.objects.filter(parent=self.cur_dir)
 
-    def list_file(self):
-        """
-        :return:информация по запрошенному файлу или ошибка
-        """
-        result = dict()
-        # родительская директория
+            # информация для навигации
+            context.update(self.navigation_inform())
+
+            context['CreateDirForm'] = self.get_form(CreateDirectoryForm)
+            context['UploadFileForm'] = self.get_form(UploadFileForm)
+            context['UpdateDirectoryNameForm'] = self.get_form(UpdateDirectoryNameForm)
+        return context
+
+    def prepare_file_context(self, context):
         self.cur_dir = self.cur_file.parent
-
-        if self.cur_file.has_access(self.request.user):
-            return {'file': self.cur_file}
+        if not self.cur_dir.has_access(self.request.user):
+            context['critical_error'] = self.errors['ACCESS_DENIED']
         else:
-            messages.add_message(self.request, messages.ERROR, 'Вам сюда доступ запрещён' )
-            return dict()
+            context['file'] = self.cur_file
+            context.update(self.navigation_inform())
 
-    def list_file(self):
-        """
-        :return:информация по запрошенному файлу или ошибка
-        """
-        result = dict()
-        # родительская директория
-        self.cur_dir = self.cur_file.parent
+            # TODO: формы другие должны быть (переименования, удаления)
+            context['CreateDirForm'] = self.get_form(CreateDirectoryForm)
+            context['UploadFileForm'] = self.get_form(UploadFileForm)
+            context['UpdateDirectoryNameForm'] = self.get_form(
+                UpdateDirectoryNameForm)
 
-        if self.cur_file.has_access(self.request.user):
-            return {'file': self.cur_file}
-        else:
-            return {'error': self.errors['ACCESS_DENIED']}
+
+            # TODO: формы другие должны быть (переименования, удаления)
+            context['CreateDirForm'] = self.get_form(CreateDirectoryForm)
+            context['UploadFileForm'] = self.get_form(UploadFileForm)
+
+        return context
 
     def get_context_data(self, **kwargs):
         path = self.kwargs['path']
         context = super(FilesView, self).get_context_data(**kwargs)
-        self.cur_dir = Directory.objects.get_by_full_path(path)
         context['messages'] = messages.get_messages(self.request)
 
+        self.cur_dir = Directory.objects.get_by_full_path(path)
         if self.cur_dir:
             # если по запрошенному пути найдена папка
-
-            # список файлов и папок в текущей директории
-            context.update(self.list_dir())
-
-            # информация для навигации
-            context.update(self.navigation_inform())
-            context['CreateDirForm'] = self.get_form(CreateDirectoryForm)
-            context['UploadFileForm'] = self.get_form(UploadFileForm)
-            context['UpdateDirectoryNameForm'] = self.get_form(UpdateDirectoryNameForm)
-
-
-            # TODO  формы удаления и переименования директории
+            context = self.prepare_dir_context(context)
         else:
             # если по запрошенному пати найден файл
             self.cur_file = File.objects.get_by_full_path(path)
             if self.cur_file:
-                context.update(self.list_file())
-                context.update(self.navigation_inform())
-                # TODO: формы другие должны быть (переименования, удаления)
-                context['CreateDirForm'] = self.get_form(CreateDirectoryForm)
-                context['UploadFileForm'] = self.get_form(UploadFileForm)
-                context['UpdateDirectoryNameForm'] = self.get_form(UpdateDirectoryNameForm)
-
+                context = self.prepare_file_context(context)
             else:
-                messages.add_message(self.request, messages.ERROR, 'Такого пути не существует' )
+                context['critical_error'] = self.errors['BAD_PATH']
 
         return context
