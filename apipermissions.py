ef3c01954f52fda920bac7fe0e033c4e89899ae2
Andrei <and-losc@mail.ru>
2015-03-16 23:26:18 +0600

    Delete apipermissions.py

--- apipermissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-from filesharing.models import Directory
-
-class UserPermission(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method == 'GET' or request.method == 'POST':
-            return obj.has_access(request.user)
-        else:
-            return obj.has_access(request.user) and obj not in Directory.objects.root_nodes()
